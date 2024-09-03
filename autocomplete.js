74e187845a7c25dd351f1ba4046ec03b74caf337
Marcus Lin <marcuslin@wifi-10-41-133-77.wifi.berkeley.edu>
2024-09-03 15:08:17 -0700

    added autocomplete.js file

---
+++ autocomplete.js
@@ -0,0 +1,4 @@
+const restaurants = [
+    "McDonald's", "Burger King", "Subway", "Starbucks", "Chipotle", "Chick-fil-A", "Taco Bell", 
+    "Wendy's", "Dunkin'", "Panda Express", "Panera Bread"
+];
